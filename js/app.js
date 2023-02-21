f60b9c1a1c2c101e36a3c9e83c896cc57eb7a432
kacperkwa <kacperkwartaxz@gmail.com>
2023-02-21 19:12:18 +0100

    added script for game results

--- js/app.js
+++ js/app.js
@@ -5,7 +5,13 @@ const playerChoiceBtn = document.querySelectorAll('.circle-btn');
 const gameBoard = document.querySelector('.game-board');
 const startGame = document.querySelector('.start-game');
 const playerImg = document.querySelector('.player-selection-img');
-
+const playAgainBtn = document.querySelector('.game-board__btn');
+const computerImage = document.querySelector('.computer-selection-img');
+const computerCircle = document.querySelector('.computer__selection');
+const playerCircle = document.querySelector('.player__selection');
+const playerImage = document.querySelector('.player-selection-img');
+let playerResult;
+let computerResult;
 //modal hanlders
 const rulesHandler = () => {
 	modal.style.display = 'flex';
@@ -17,33 +23,35 @@ const closeModal = () => {
 //game engine
 const playerPick = e => {
 	const playerChoiceId = e.target.closest('button').id;
-	const circle = document.querySelector('.player__selection');
-	const image = document.querySelector('.player-selection-img');
 	if (playerChoiceId === 'paper') {
-		image.src = '/images/icon-paper.svg';
-		circle.classList.add('paper-styles');
-	} else if (playerChoiceId === 'scisorss') {
-		image.src = '/images/icon-scissors.svg';
-		circle.classList.add('scissors-styles');
+		playerImage.src = '/images/icon-paper.svg';
+		playerCircle.classList.add('paper-styles');
+		playerResult = 'paper';
+	} else if (playerChoiceId === 'scissors') {
+		playerImage.src = '/images/icon-scissors.svg';
+		playerCircle.classList.add('scissors-styles');
+		playerResult = 'scissors';
 	} else {
-		image.src = '/images/icon-rock.svg';
-		circle.classList.add('rock-styles');
+		playerImage.src = '/images/icon-rock.svg';
+		playerCircle.classList.add('rock-styles');
+		playerResult = 'rock';
 	}
 };
 const computerPick = () => {
-	const image = document.querySelector('.computer-selection-img');
-	const circle = document.querySelector('.computer__selection');
-	const arr = ['paper', 'scisorss', 'rock'];
+	const arr = ['paper', 'scissors', 'rock'];
 	const computerChoice = arr[Math.floor(Math.random() * arr.length)];
 	if (computerChoice === 'paper') {
-		image.src = '/images/icon-paper.svg';
-		circle.classList.add('paper-styles');
-	} else if (computerChoice === 'scisorss') {
-		image.src = '/images/icon-scissors.svg';
-		circle.classList.add('scissors-styles');
+		computerImage.src = '/images/icon-paper.svg';
+		computerCircle.classList.add('paper-styles');
+		computerResult = 'paper';
+	} else if (computerChoice === 'scissors') {
+		computerImage.src = '/images/icon-scissors.svg';
+		computerCircle.classList.add('scissors-styles');
+		computerResult = 'scissors';
 	} else {
-		image.src = '/images/icon-rock.svg';
-		circle.classList.add('rock-styles');
+		computerImage.src = '/images/icon-rock.svg';
+		computerCircle.classList.add('rock-styles');
+		computerResult = 'rock';
 	}
 };
 const gameHandler = e => {
@@ -52,10 +60,42 @@ const gameHandler = e => {
 	playerPick(e);
 	computerPick();
 };
+const playAgain = () => {
+	gameBoard.style.display = 'none';
+	startGame.style.display = 'flex';
+	const computerItemToDelete = Array.from(computerCircle.classList).pop();
+	const playerItemToDelete = Array.from(playerCircle.classList).pop();
+	computerCircle.classList.remove(computerItemToDelete);
+	playerCircle.classList.remove(playerItemToDelete);
+};
+const gameResult = () => {
+	const resultText = document.querySelector('.game-board__result');
+	const scoreBoard = document.querySelector('.scoreboard__score');
+	let gameScore = 0;
+	scoreBoard.textContent = `${gameScore}`;
+	if (playerResult === computerResult) {
+		resultText.textContent = `it's a draw`;
+	} else if (
+		(playerResult === 'paper' && computerResult === 'rock') ||
+		(playerResult === 'scissors' && computerResult === 'paper') ||
+		(playerResult === 'rock' && computerResult === 'scissors')
+	) {
+		resultText.textContent = `you win`;
+		gameScore += 1;
+		console.log(gameScore);
+	} else {
+		resultText.textContent = `you lose`;
+		gameScore -= 1;
+		console.log(gameScore);
+	}
+};
+
 rulesBtn.addEventListener('click', rulesHandler);
 closeModalBtn.addEventListener('click', closeModal);
 playerChoiceBtn.forEach(btn => {
 	btn.addEventListener('click', e => {
 		gameHandler(e);
+		gameResult();
 	});
 });
+playAgainBtn.addEventListener('click', playAgain);
